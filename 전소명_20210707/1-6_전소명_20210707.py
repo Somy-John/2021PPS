6223a173d18d280a5209acba2d746fd75c697cf2
Somy-John <john621145@gmail.com>
2021-07-07 16:37:08 +0900

    day3-1

--- 전소명_20210707/1-6_전소명_20210707.py
+++ 전소명_20210707/1-6_전소명_20210707.py
@@ -5,8 +5,7 @@ def generate(numRows):
         elif i==1 : rst.append([1,1])
         else:
             tmp = [1]
-            for j in range(len(rst[-1])-1):
-                tmp.append(rst[-1][j]+rst[-1][j+1])
+            for j in range(len(rst[-1])-1): tmp.append(rst[-1][j]+rst[-1][j+1])
             tmp.append(1)
             rst.append(tmp)
     return rst
