a5b53b02774edf1022800b8204b4a363f041e898
Somy-John <john621145@gmail.com>
2021-07-14 11:42:53 +0900

    20210714

--- 전소명_20210713/6-2_전소명_20210713.py
+++ 전소명_20210713/6-2_전소명_20210713.py
@@ -1,5 +1,3 @@
-n,k=map(int,input().split())
-*p,i=list(range(1,n+1))+[0]
-print("<",end='')
-while len(p)>1:print(p.pop(i := i+k-1 if (i+k-1)<len(p) else (i+k-1)%len(p)),end=", ")
+n,k=map(int,input().split());*p,i=list(range(1,n+1))+[0];print("<",end='')
+while len(p)>1:print(p.pop(i:=(i+k-1)%len(p)),end=", ")
 print(f'{p[0]}>')
